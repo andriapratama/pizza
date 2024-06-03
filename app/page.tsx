1268208eeab000adace3da95ce96f1fa10820f34
Andria Pratama <andrilocong1st@gmail.com>
2024-06-03 17:34:36 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
 import { WaveBg } from '@/constants/backgrounds';
+import { Pizza1 } from '@/constants/pizzas';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="w-full overflow-hidden bg-black relative h-screen">
-      <div className="absolute w-[60%] border-2 border-white rounded-full left-[50%] bottom-0 aspect-[1/1] translate-y-1/2 -translate-x-1/2"></div>
-      <div className="absolute w-[55%] border-2 border-white rounded-full left-[50%] bottom-0 aspect-[1/1] translate-y-1/2 -translate-x-1/2"></div>
+      <div className="absolute w-[60%] border-2 border-white rounded-full left-1/2 bottom-0 aspect-[1/1] translate-y-1/2 -translate-x-1/2"></div>
+      <div className="absolute w-[55%] border-2 border-white rounded-full left-1/2 bottom-0 aspect-[1/1] translate-y-1/2 -translate-x-1/2"></div>
       <Image
         src={WaveBg}
         alt="wave bg"
         width="0"
         height="0"
         className="object-contain w-full h-auto absolute -bottom-[100px] left-0"
+      />
+      <Image
+        src={Pizza1}
+        alt="pizza"
+        width="0"
+        height="0"
+        className="w-1/2 left-1/2 -translate-x-1/2 bottom-0 translate-y-1/2 aspect-[1/1] absolute"
       />
     </main>
   );
---
+++ constants/pizzas.ts
@@ -0,0 +1,3 @@
+import Pizza1 from '../public/pizza/3799722_10219.png';
+
+export { Pizza1 };
